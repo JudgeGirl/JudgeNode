36cce8e911b9153d918c7534e3cabadf68a1362b
unknown <morris821028@gmail.com>
2015-12-13 11:35:59 +0800

    avatar upload without modify

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 var dblink = require('../lib/components/dblink');
 var config = require('../lib/config').config;
+var MAU = require('../lib/components/modify-and-upload');
 var multer = require('multer');
-var upload = multer({dest: 'files/'});
 var fs = require('fs');
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+    	cb(null, './files/')
+  	},
+  	filename: function (req, file, cb) {
+	    cb(null, Date.now() + file.originalname) //Appending .jpg
+  	}
+})
+var upload = multer({
+	dest: './files/',
+  	limits: {
+       fileSize: 1024 * 1024
+    },
+    storage: storage
+});
 
 /* GET users page */
 router.get('/', function(req, res, next) {
@@ -18,6 +33,19 @@ router.get('/:uid', function(req, res, next) {
 	dblink.user.info(uid, function(user) {
 		res.render('layout', { layout: 'user', subtitle: 'User', user: req.session, userinfo: user});
 	});
+});
+
+/*  */
+router.post('/upload/avatar', 
+	upload.fields([{name: 'avatar', maxCount: 1}]), function(req, res, next) {
+	var uid = req.session.uid;
+	if (uid == undefined || uid == null) {
+		res.redirect('../login');
+	} else {
+		var mau = new MAU(req.files['avatar'][0], uid, function(err, newImagePath){
+	    	res.redirect('/');
+		});
+	}
 });
 
 module.exports = router;
