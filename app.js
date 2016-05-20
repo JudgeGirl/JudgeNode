162739e8f12eb2b1453c4453b084b15d05f161e4
root <morris821028@gmail.com>
2016-05-20 13:17:54 +0800

    fixed i18n in use order

--- app.js
+++ app.js
@@ -21,36 +21,24 @@ i18n.configure({
   // setup some locales - other locales default to en silently
   locales: ['en', 'zhtw'],
 
-  defaultLocale: ['zhtw'],
+  defaultLocale: ['en'],
   // sets a custom cookie name to parse locale settings from
   cookie: 'locale',
  
   // where to store json files - defaults to './locales'
   directory: __dirname + '/locales'
 });
-
-app.use(i18n.init);
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.locals.__ = res.__ = function() {
-        return i18n.__.apply(req, arguments);
-    };
     next();
 });
-// app.use (function (req, res, next) {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     res.redirect('https://' + req.headers.host + req.url);
-//   }
-// });
-app.use(function (req, res, next) {
-    res.locals.site = _config;
-    res.locals.site.unitConvert = utils.unitConvert,
-    res.locals.site.url_for = utils.url_for,
-    next();
-});
-
+app.use (function (req, res, next) {
+   if (req.secure) {
+     next();
+   } else {
+     res.redirect('https://' + req.headers.host + req.url);
+   }
+ });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -62,31 +50,44 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(i18n.init);
 app.use(session({
   cookie: {
     path    : '/',  // important !!
     httpOnly: false,
-    maxAge : 24*60*60*1000
+    maxAge : 6*60*60*1000
   },
   secret: 'alskdjasjoimk'
 }));
+app.use(function (req, res, next) {
+	res.locals.__ = res.__ = function() {
+		return i18n.__.apply(req, arguments);
+	};
+
+	res.locals.site = _config;
+	res.locals.site.unitConvert = utils.unitConvert,
+	res.locals.site.url_for = utils.url_for,
+	next();
+});
+
 app.use(utils.url_for('/'), express.static(path.join(__dirname, 'public')));
 
 app.use(utils.url_for('admin'), admins);
 app.use(utils.url_for('user'), users);
 app.use(utils.url_for('/'), routes);
 app.get(utils.url_for('/i18n/:locale'), function (req, res) {
-    res.cookie('locale', req.params.locale);
-    i18n.setLocale(req.params.locale);
-    if (req.headers.referer) res.redirect(req.headers.referer);
-    else res.redirect(utils.url_for('/'));
+	res.cookie('locale', req.params.locale);
+	req.session.locale = req.params.locale;
+	//    i18n.setLocale(req, req.params.locale);
+	if (req.headers.referer) res.redirect(req.headers.referer);
+	else res.redirect(utils.url_for('/'));
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -94,25 +95,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-      user: null
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err,
+			user: null
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {},
-    user: null
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {},
+		user: null
+	});
 });
 
 
