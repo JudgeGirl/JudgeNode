2b7a731214e3e62c9745ee2f9a15cd709482b8e7
TA <dplabta@gmail.com>
2020-01-01 08:39:18 +0800

    update config for scripts with comment

--- test/workspace/calculate_score/config.js
+++ test/workspace/calculate_score/config.js
@@ -1,34 +1,38 @@
 const config = {
-    weekNumber: "15",                            // for building path
-    gradeId: "91",
+    weekNumber: "17",                            // for building path
+    gradeId: "93",
     cid: {
-        Mon: "193",
-        Tue: "194"
+        Mon: "197",
+        Tue: "198"
     },
     studentNumber: {
-        Mon: 81,                             // for calculating average
-        Tue: 48
+        Mon: 73,                                // for calculating average
+        Tue: 47
+    },
+    merge: {
+        Mon: [0, 1],                            // we merge scores of two problems for some exam
+        Tue: [0, 1]                             // leave empty array([]) if not needed
     },
     scale: {
         Mon: {
             problemList: [
-                "50105"
-            ],                                  // can be an arrya of problems
+                "50148"
+            ],                                  // can be an array of problems
             passLimit: 60,                      // quilified students have score higher than this
             passScale: 1.0,                     // scale for student qualified
             failScale: 0.5,                     // scale for student not qualified
             studentClass: 2,                    // class 2 for Mon, class 1 for Tue
-            deadline: "2019-12-15 10:30:00"     // "yyyy-mm-dd HH:ii:ss"
+            deadline: "2019-12-29 10:30:00"     // "yyyy-mm-dd HH:ii:ss"
         },
         Tue: {
             problemList: [
-                "50105"
+                "50148"
             ],
             passLimit: 60,
             passScale: 1.0,
             failScale: 0.5,
             studentClass: 1,
-            deadline: "2019-12-16 15:30:00"
+            deadline: "2019-12-30 15:30:00"
         },
         filename: "scaleTable.csv"
     },
