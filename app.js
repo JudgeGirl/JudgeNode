d8bc1590467d7619f837cff084bfff47ce9b7439
TA <dplabta@gmail.com>
2020-03-11 15:51:06 +0800

    use env in config file to determine whether or not display error message

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if ( _config.JUDGE.env === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
