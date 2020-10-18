bcc220f11691833119de9090990345608ecea613
Daniel1147 <r08922169@ntu.edu.tw>
2020-10-18 14:11:11 +0800

    require permission to view reports

--- routes/report.js
+++ routes/report.js
@@ -2,26 +2,48 @@ var express = require('express');
 var router = express.Router();
 var dblink = require('../lib/components/dblink');
 
-router.get('/:sid', function(req, res, next) {
-    var sid = req.params.sid;
+function renderReportContent(content) {
+    return content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+}
 
+function buildReportLayout(report) {
+    return {
+        layout: 'report',
+        report_raw: report
+    };
+}
+
+function buildErrorLayout(message) {
+    return {
+        message: message,
+        error: {
+            status: "",
+            stack: ""
+        }
+    };
+}
+
+router.get('/:sid', async function(req, res, next) {
+    let sid = req.params.sid;
+    let uid = req.session.uid;
+    let viewPrivilege = await dblink.permission.sourceCodeVeiwPrivilege(uid, sid);
+
+    if (!viewPrivilege) {
+        res.render('layout', {
+            layout: 'report',
+            report_raw: 'You neet to pass this problem to view the report.'
+        });
+
+        return;
+    }
 
     dblink.report.getBySidPromise(sid).then(reportRow => {
         let report = reportRow[0].content
-        report = report.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        report = renderReportContent(report);
 
-        res.render('layout', {
-            layout: 'report',
-            report_raw: report
-        });
+        res.render('layout', buildReportLayout(report));
     }).catch(error => {
-        res.render('error', {
-            message: error,
-            error: {
-                status: "",
-                stack: ""
-            }
-        })
+        res.render('error', buildErrorLayout(error));
     });
 
 });
