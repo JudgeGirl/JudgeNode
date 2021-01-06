98c479118bf219c50798ff1cafd85bbc23fe15cd
Daniel1147 <r08922169@ntu.edu.tw>
2021-01-07 03:36:08 +0800

    Add DictionaryCacheResource.

---
+++ test/lib/components/resource/cacheResource.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect;
+
+const RedisCacheResource = require('lib/components/resource/RedisCacheResource');
+const DictionaryCacheResource = require('lib/components/resource/DictionaryCacheResource');
+const StaticCacheKeyFactory = require('lib/components/resource/cacheKeyFactory/StaticCacheKeyFactory');
+const redisClient = require('lib/components/RedisClient');
+
+const cacheKeyFactory = new StaticCacheKeyFactory('test');
+const content = 'hello 你好';
+
+describe("cache resources", async function() {
+    it("RedisCacheResource", async function() {
+        await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
+
+        let response;
+        let resource = new RedisCacheResource(cacheKeyFactory, 10);
+        response = await resource.get();
+        expect(response).to.equal(null);
+
+        await resource.set(null, content);
+        response = await resource.get();
+        expect(response).to.equal(content);
+
+        await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
+    });
+
+    it('DictionaryCacheResource', async function() {
+        let resource = new DictionaryCacheResource(cacheKeyFactory);
+        let response;
+
+        response = await resource.get();
+        expect(response).to.equal(null);
+
+        await resource.set(null, content);
+        response = await resource.get();
+        expect(response).to.equal(content);
+
+        await resource.invalidateKey();
+        response = await resource.get();
+        expect(response).to.equal(null);
+    })
+});
