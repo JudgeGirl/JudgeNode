80c629e9cbfa990d836a09459c4bd150a0b741ce
Daniel1147 <r08922169@ntu.edu.tw>
2021-01-06 15:53:58 +0800

    Add FileLocators.

---
+++ test/lib/components/resource/fileLocator.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+
+const RawFileLocator = require('lib/components/resource/fileLocator/RawFileLocator');
+const StaticFileLocator = require('lib/components/resource/fileLocator/StaticFileLocator');
+const TemplateFileLocator = require('lib/components/resource/fileLocator/TemplateFileLocator');
+
+describe("file locators", function(){
+    it("RawFileLocator", function(){
+        const fileLocator = new RawFileLocator();
+
+        let key = "file-path";
+
+        expect(fileLocator.getFilePath(key)).to.equal(key);
+    });
+
+    it("StaticFileLocator", function(){
+        const staticFile = 'file.txt'
+        const fileLocator = new StaticFileLocator(staticFile);
+
+        let key = "file-path";
+
+        expect(fileLocator.getFilePath(key)).to.equal(staticFile);
+    });
+
+    it("TemplateFileLocator", function(){
+        const fileLocator = new TemplateFileLocator('path/../', '.extension');
+
+        let key = "name";
+
+        expect(fileLocator.getFilePath(key)).to.equal('path/../name.extension');
+    });
+});
