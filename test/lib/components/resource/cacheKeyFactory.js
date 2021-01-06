f4fd2889954f10cdc1fad6ffa95d0088e8b4c4f3
Daniel1147 <r08922169@ntu.edu.tw>
2021-01-06 17:26:49 +0800

    Add CacheKeyFactories.

---
+++ test/lib/components/resource/cacheKeyFactory.js
@@ -0,0 +1,23 @@
+const expect = require('chai').expect;
+
+const StaticCacheKeyFactory = require('lib/components/resource/cacheKeyFactory/StaticCacheKeyFactory');
+const TemplateCacheKeyFactory = require('lib/components/resource/cacheKeyFactory/TemplateCacheKeyFactory');
+
+describe("file locators", function(){
+    it("StaticCacheKeyFactory", function(){
+        const staticKey = 'announcement.md'
+        const cacheKeyFactory = new StaticCacheKeyFactory(staticKey);
+
+        let key = "whatever";
+
+        expect(cacheKeyFactory.getCacheKey(key)).to.equal(`cache:${staticKey}`);
+    });
+
+    it("TemplateCacheKeyFactory", function(){
+        const cacheKeyFactory = new TemplateCacheKeyFactory('problem', '.md');
+
+        let key = "50123";
+
+        expect(cacheKeyFactory.getCacheKey(key)).to.equal('cache-problem:50123.md');
+    });
+});
