c2f98239ba376aa89377ad9387f6ca0359d30062
Daniel1147 <r08922169@ntu.edu.tw>
2021-01-10 00:21:02 +0800

    Add LineToListConverter.

--- test/lib/components/resource/resourceConverter.js
+++ test/lib/components/resource/resourceConverter.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 
 const ToJsonConverter = require('lib/components/resource/resourceConverter/ToJsonConverter');
 const FromJsonConverter = require('lib/components/resource/resourceConverter/FromJsonConverter');
+const LineToListConverter = require('lib/components/resource/resourceConverter/LineToListConverter');
 const StaticResource = require('lib/components/resource/StaticResource');
 
 describe("resource converter", function () {
@@ -15,6 +16,27 @@ describe("resource converter", function () {
         let converter = new FromJsonConverter();
 
         expect(converter.convert('[123]')).to.deep.equal([123]);
+    });
+
+    it("LineToListConverter", function () {
+        let converter = new LineToListConverter();
+
+        expect(converter.convert('a\nb\nc')).to.deep.equal(['a', 'b', 'c']);
+        expect(converter.convert('a\nb\nc\n')).to.deep.equal(['a', 'b', 'c']);
+        expect(converter.convert('a\n b\nc\n')).to.deep.equal(['a', 'b', 'c']);
+        expect(converter.convert('a\n b \nc\n')).to.deep.equal(['a', 'b', 'c']);
+        expect(converter.convert('')).to.deep.equal([]);
+        expect(converter.convert(null)).to.deep.equal([]);
+        expect(() => converter.converter()).to.throw();
+        expect(() => converter.converter(123)).to.throw();
+    });
+
+    it('LineToListConverter in resource.', async function() {
+        const lines = 'set.c\nset.h\n';
+        const resource = new StaticResource(lines);
+        resource.setOutputConverter(new LineToListConverter());
+
+        expect(await resource.get()).to.deep.equal(['set.c', 'set.h']);
     });
 
     it("In resource.", async function() {
