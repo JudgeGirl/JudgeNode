3515b9c5d0f01d2d71adbc382fde26e37f3e10a7
Daniel1147 <r08922169@ntu.edu.tw>
2021-01-10 18:14:19 +0800

    Remove a test that contains operations to Redis.

--- test/lib/components/resource/cacheResource.js
+++ test/lib/components/resource/cacheResource.js
@@ -1,30 +1,30 @@
 const expect = require('chai').expect;
 
-const RedisCacheResource = require('lib/components/resource/RedisCacheResource');
+// const RedisCacheResource = require('lib/components/resource/RedisCacheResource');
 const DictionaryCacheResource = require('lib/components/resource/DictionaryCacheResource');
 const StaticCacheKeyFactory = require('lib/components/resource/cacheKeyFactory/StaticCacheKeyFactory');
-const { redisClient } = require('lib/components/RedisClient');
+// const { redisClient } = require('lib/components/RedisClient');
 
 const cacheKeyFactory = new StaticCacheKeyFactory('test-redis-cache');
 const content = 'hello 你好';
 
 describe("cache resources", async function() {
-    it("RedisCacheResource", async function() {
-        await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
-
-        let response;
-        let resource = new RedisCacheResource(cacheKeyFactory, 100);
-        response = await resource.get();
-        expect(response).to.equal(null);
-
-        await resource.set(null, content);
-        response = await resource.get();
-        expect(response).to.equal(content);
-
-        await resource.refreshKey();
-
-        await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
-    });
+    // it("RedisCacheResource", async function() {
+    //     await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
+    //
+    //     let response;
+    //     let resource = new RedisCacheResource(cacheKeyFactory, 100);
+    //     response = await resource.get();
+    //     expect(response).to.equal(null);
+    //
+    //     await resource.set(null, content);
+    //     response = await resource.get();
+    //     expect(response).to.equal(content);
+    //
+    //     await resource.refreshKey();
+    //
+    //     await redisClient.deleteKey(cacheKeyFactory.getCacheKey());
+    // });
 
     it('DictionaryCacheResource', async function() {
         let resource = new DictionaryCacheResource(cacheKeyFactory);
