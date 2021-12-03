136ee1879a0177683b55aee50e3cf80ac8ed7708
Daniel1147 <r08922169@ntu.edu.tw>
2021-12-03 13:42:34 +0800

    add solved list page

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ var config = require('../lib/config').config;
 var MAU = require('../lib/components/modify-and-upload');
 var multer = require('multer');
 var utils = require('../lib/components/utils');
+var helper = require('./helper');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -14,12 +15,78 @@ var storage = multer.diskStorage({
         cb(null, Date.now() + file.originalname) //Appending .jpg
     }
 })
+
 var upload = multer({
     dest: './files/',
     limits: {
         fileSize: 1024 * 1024
     },
     storage: storage
+});
+
+/* sovled list */
+router.get('/solved-list', function(req, res, next) {
+    let uid = req.session.uid;
+
+    if (uid == undefined) {
+        res.redirect(utils.url_for('/'));
+        return;
+    }
+
+    res.redirect(utils.url_for(`/user/${uid}/solved-list`));
+});
+
+router.get('/:uid/solved-list', async function(req, res, next) {
+    let uid = req.params.uid;
+    let selfId = req.session.uid;
+    let isTestMode = config.CONTEST.MODE == true;
+    let hasNoClass = req.session['class'] == null;
+    let isAdmin = await dblink.user.promises.isAdmin(selfId);
+
+    // identity check
+    let uidAvailable = uid.match(/^[0-9]+$/) !== null;
+    if (!uidAvailable) {
+        return helper.renderForbidden({
+            res,
+            message: "Invalid uid."
+        })
+    }
+
+    // fetch user
+    let user = await dblink.user.promises.getUser(uid);
+    if (user.length == 1)
+        user = user[0];
+    else {
+        return helper.renderForbidden({
+            res,
+            message: "Invalid user."
+        })
+    }
+
+    // mode check
+    if (isTestMode && !isAdmin) {
+        return res.render('layout', {
+            layout: 'forbidden',
+            subtitle: 'Forbidden',
+            sysmsg: 'This page is not availabe during examination.'
+        });
+    }
+
+    // display mode: private/public
+    let privateMode = false;
+    if (isAdmin)
+        privateMode = true;
+    else if (!hasNoClass && uid == selfId)
+        privateMode = true;
+
+    let solvedList = await dblink.user.promises.getSolvedList(uid, privateMode);
+
+    return res.render('layout', {
+        layout: 'solved_list',
+        lgn: user["lgn"],
+        uid: uid,
+        solvedList
+    });
 });
 
 /* GET users page */
